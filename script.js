e54b86cea1907a06a6dfecd6a2649bf88b64691c
aadic <aadi.charpe@hotmail.com>
2025-02-15 09:13:25 -0800

    it works :D

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function buttonClick() {
+async function buttonClick() {
     let ta = document.getElementById("ta");
-    console.log(ta.value)
+    let mtext = document.getElementById("misspelled");
+    let text = ta.value;
+    await fetch("http://127.0.0.1:5000/spell_check", {method: "POST", body: JSON.stringify({"message": text}), headers: {'Content-Type': 'application/json'}})
+    .then((response)=>{
+        mtext.value = "";
+        response.json().then((data)=>{
+            for(let i = 0; i < data["incorrect"].length; i++)
+                mtext.value += data["incorrect"][i] + '\n';
+        })
+    })
 }
